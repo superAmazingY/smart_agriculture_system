d9134249224926bbf752c76b360c55779f4cd936
superAmazingY <puyinglee1020@gmail.com>
2023-03-07 19:02:42 +0800

    smart_agriculture_system 1.0.1 modify URL

--- backend/HTTP/controllers/server_api.h
+++ backend/HTTP/controllers/server_api.h
@@ -8,16 +8,16 @@ class User : public drogon::HttpController<User> {
 public:
     METHOD_LIST_BEGIN
         // use METHOD_ADD to add your custom processing function here;
-        METHOD_ADD(User::loginInfo, "login/userId={1}&passwd={2}",Post,Options);  // url:  http://127.20.10.6:9998/user/login/userId={1}&passwd={2}   (登录API)
-        METHOD_ADD(User::forgetPasswordInfo, "forgetPassword/userId={1}&passwd={2}",Post,Options);  // url:  http://127.20.10.6:9998/user/forgetPassword/userId={1}&passwd={2}  (忘记密码API)
-        METHOD_ADD(User::registerInfo, "register/userId={1}&passwd={2}",Post,Options);  // url:  http://127.20.10.6:9998/user/register/userId={1}&passwd={2}   (注册API)
-        METHOD_ADD(User::getNitrogenInfo, "/nitrogenInfo",Get,Options);              // url:  http://127.20.10.6:9998/user/nitrogenInfo     (氮元素API)
-        METHOD_ADD(User::getPhosphorusInfo, "/phosphorusInfo",Get,Options);              // url:  http://127.20.10.6:9998/user/phosphorusInfo   (磷元素API)
-        METHOD_ADD(User::getPotassiumInfo, "/potassiumInfo",Get,Options);              // url:  http://127.20.10.6:9998/user/potassiumInfo     (钾元素API)
-        METHOD_ADD(User::getTemperatureInfo, "/temperatureInfo",Get,Options);              // url:  http://127.20.10.6:9998/user/temperatureInfo   (温度API)
-        METHOD_ADD(User::getHumidityInfo, "/humidityInfo",Get,Options);              // url:  http://127.20.10.6:9998/user/humidityInfo       (湿度API)
-        METHOD_ADD(User::getPhValueInfo, "/phValueInfo",Get,Options);              // url:  http://127.20.10.6:9998/user/phValueInfo         (PH值API)
-        METHOD_ADD(User::getErrorInfo, "/errorInfo",Get,Options);              // url:  http://127.20.10.6:9998/user/errorInfo        (错误提示API)
+        METHOD_ADD(User::loginInfo, "login?userId={1}&passwd={2}",Post,Options);  // url:  http://127.20.10.6:9998/user/login?userId={1}&passwd={2}   (登录API)
+        METHOD_ADD(User::forgetPasswordInfo, "forgetPassword?userId={1}&passwd={2}",Post,Options);  // url:  http://127.20.10.6:9998/user/forgetPassword?userId={1}&passwd={2}  (忘记密码API)
+        METHOD_ADD(User::registerInfo, "register?userId={1}&passwd={2}",Post,Options);  // url:  http://127.20.10.6:9998/user/register?userId={1}&passwd={2}   (注册API)
+        METHOD_ADD(User::getNitrogenInfo, "nitrogenInfo",Get,Options);              // url:  http://127.20.10.6:9998/user/nitrogenInfo     (氮元素API)
+        METHOD_ADD(User::getPhosphorusInfo, "phosphorusInfo",Get,Options);              // url:  http://127.20.10.6:9998/user/phosphorusInfo   (磷元素API)
+        METHOD_ADD(User::getPotassiumInfo, "potassiumInfo",Get,Options);              // url:  http://127.20.10.6:9998/user/potassiumInfo     (钾元素API)
+        METHOD_ADD(User::getTemperatureInfo, "temperatureInfo",Get,Options);              // url:  http://127.20.10.6:9998/user/temperatureInfo   (温度API)
+        METHOD_ADD(User::getHumidityInfo, "humidityInfo",Get,Options);              // url:  http://127.20.10.6:9998/user/humidityInfo       (湿度API)
+        METHOD_ADD(User::getPhValueInfo, "phValueInfo",Get,Options);              // url:  http://127.20.10.6:9998/user/phValueInfo         (PH值API)
+        METHOD_ADD(User::getErrorInfo, "errorInfo",Get,Options);              // url:  http://127.20.10.6:9998/user/errorInfo        (错误提示API)
     METHOD_LIST_END
 
     // POST请求函数实现虚函数
